0f85b069354654fc649815632ba7064575e6e721
Matheus <alvesdemelomatheus4@gmail.com>
2026-05-23 00:05:16 -0300

    feat(router): adicionar rota de checkout

--- lib/globals/router.dart
+++ lib/globals/router.dart
@@ -4,6 +4,7 @@ import 'package:nhac/pages/bem_vindo.dart';
 import 'package:nhac/pages/auth/continuar_senha.dart';
 import 'package:nhac/pages/auth/email_cliente.dart';
 import 'package:nhac/pages/auth/insira_telefone.dart';
+import 'package:nhac/pages/checkout_page.dart';
 import 'package:nhac/pages/splash_screen.dart';
 import 'package:nhac/pages/bem_vindo_motoca.dart';
 import 'package:nhac/pages/auth/verificacao_numero.dart';
@@ -272,5 +273,12 @@ final GoRouter appRouter = GoRouter(
         child: const CuponsPage(),
       ),
     ),
+    GoRoute(
+  path: '/checkout',
+  pageBuilder: (context, state) => _buildSlideRightToLeftPage(
+    key: state.pageKey,
+    child: const CheckoutPage(),
+  ),
+),
   ],
 );
