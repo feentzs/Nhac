7f7bf5f9c5e757dc00564fb87fa47c0b14db6564
ftz*! <68380939+feentzs@users.noreply.github.com>
2026-09-02 10:52:33 -0300

    Corrigido erro na build com a depedencia do Google Maps | Suporte adicionado para o Android 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     // END: FlutterFire Configuration
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
-    
 }
 
 val keystoreProperties = Properties()
@@ -19,14 +18,14 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.feentzs.nhac"
-    compileSdk = flutter.compileSdkVersion
+    // Defina no máximo 35 (Android 15) para garantir compatibilidade completa das bibliotecas
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
         isCoreLibraryDesugaringEnabled = true
-        
     }
 
     kotlin {
@@ -37,10 +36,8 @@ android {
 
     defaultConfig {
         applicationId = "com.feentzs.nhac"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 37
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
